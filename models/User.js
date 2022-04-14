75ab6c751e4661c4401da53a1cc5f09e81de6acf
Rodney P <rodney.plancher@gmail.com>
2022-04-13 20:13:33 -0400

    added user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,37 @@
 const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must be an Email']
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+},
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+)
+const User = model('User', userSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Use this to log mongo queries being executed!
+mongoose.set("debug", true);
+
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
