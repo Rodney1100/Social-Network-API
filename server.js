05d0d87fa997f1155a39f521bd8b8eb6d28d6c34
Rodney P <rodney.plancher@gmail.com>
2022-04-13 23:36:06 -0400

    added the thought controller

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/social-network", {
 });
 // Use this to log mongo queries being executed!
 mongoose.set("debug", true);
+app.use(require("./routes"));
 
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
